665a6327b61e19b9d1fd801b99b6c5a133ba52b5
邓东亚的修行账号 <qfkx@msn.com>
2022-11-04 14:27:14 +0800

    整理常用的库。

--- common/utils.go
+++ common/utils.go
@@ -4,9 +4,10 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
-	"github.com/google/uuid"
 	"os"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 // UUIDString 生成32位UUID
@@ -24,25 +25,12 @@ func GetNanoTimeLabel() (cur string) {
 	return
 }
 
-// base64x
-func base64x() string {
-	ff, _ := os.Open("d:/車牌.jpg")
+// Base64X
+func Base64X(fileName string) string {
+	ff, _ := os.Open(fileName)
 	defer ff.Close()
 	buf := make([]byte, 600000)
 	n, _ := ff.Read(buf)
 	res := base64.StdEncoding.EncodeToString(buf[:n])
 	return res
-	/*
-		//写入临时文件
-		ioutil.WriteFile("a.png.txt", []byte(sourcestring), 0667)
-		//读取临时文件
-		cc, _ := ioutil.ReadFile("a.png.txt")
-
-		//解压
-		dist, _ := base64.StdEncoding.DecodeString(string(cc))
-		//写入新文件
-		f, _ := os.OpenFile("xx.png", os.O_RDWR|os.O_CREATE, os.ModePerm)
-		defer f.Close()
-		f.Write(dist)
-	*/
 }
